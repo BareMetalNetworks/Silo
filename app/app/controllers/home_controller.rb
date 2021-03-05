be96da97aa77bff99a2dda438d24c9cde465ed43
Samuel Kerr <transiencymail@gmail.com>
2021-03-05 06:11:58 -0700

    only showing produce that is available:true in home controller

--- app/app/controllers/home_controller.rb
+++ app/app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
   	@dividerIcon = 'fas fa-tractor'
-  	@produces = Produce.order(:name)
+  	@produces = Produce.order(:name).where(available: true)
   end
 end
