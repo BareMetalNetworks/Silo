ff532c087114c12cfd447aebb11322d6e7c04c0d
Samuel Kerr <transiencymail@gmail.com>
2021-03-09 06:32:48 -0700

    redirect to user profile on successful login

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :produces
   devise_for :users
   root to: 'home#index'
+  get '/users/edit' => "users#edit", :as => :user_root
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
